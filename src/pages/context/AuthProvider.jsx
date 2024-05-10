c793419800269b7c9cf70477c7902aeabe70d711
ABid hasan <abeydhasan134@gmail.com>
2024-05-11 02:43:34 +0600

    registerpage

--- src/pages/context/AuthProvider.jsx
+++ src/pages/context/AuthProvider.jsx
@@ -1,11 +1,22 @@
+import { createUserWithEmailAndPassword } from 'firebase/auth'
 import React, { createContext, useState } from 'react'
+import { auth } from '../../firebase/firebase'
 
 export const AuthContext=createContext()
 
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null)
     const [loading, setLoading]=useState(true)
-    const authInfo={user,setUser,loading,setLoading}
+
+    const createUser=(email,password)=>{
+        // use firebase build in functions
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth,email,password)
+        // when user is createing loading should be true 
+      }
+
+    const authInfo={user,setUser,loading,setLoading,createUser}
+    
   return (
     <AuthContext.Provider value={authInfo}>
         {children}
--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -2,6 +2,7 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../layout/App";
 import Home from "../pages/Home/Home";
+import Register from "../register/register";
 const Routers = createBrowserRouter([
     {
       path: "/",
@@ -10,7 +11,12 @@ const Routers = createBrowserRouter([
         {
             path: "/",
             element: <Home></Home>
+        },
+        {
+          path: "/register",
+          element: <Register></Register>
         }
+
       ]
     },
   ]);
